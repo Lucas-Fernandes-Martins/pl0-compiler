17ea311fbc22e65714b6c3b79cf1a49a7e13ba1b
Lucas-Fernandes-Martins <lucasfmartins16@gmail.com>
2025-04-06 22:33:30 -0400

    adding state machine base code

--- src/main.c
+++ src/main.c
@@ -2,6 +2,56 @@
 #include<stdlib.h>
 
 #define N_RESERVED_WORDS 11
+#define N_CONSUMABLE_CHARS 1
+#define N_OPERATORS 7
+#define N_STATES 1
+#define N_CHARS 50
+
+typedef struct {
+	int number;
+	char char_consumed;
+} State;
+
+//Moore's machine state transition output;
+typedef struct {
+	int next_state;
+	char output;
+} Output;
+
+
+int char_index(char type) {
+    return type - 'A';
+}
+
+int hash_function(int state, char char_consumed){
+	return state*N_STATES + char_index(char_consumed);
+}
+
+void lexical_analyser(char *string){
+	int number = 0;
+	
+	//Initialize hash table
+	Output *hash_table = malloc(sizeof(Output)*N_STATES*N_CHARS);
+
+	Output output1;
+	output1.next_state = 1;
+	output1.output = 'a';
+
+	hash_table[0] = output1;	
+	
+	int is_final_state = 0;
+	int i = 0;
+	int current_state = 0;
+	while(!is_final_state){
+		char char_consumed = string[i];
+		int hash_value = hash_function(current_state, char_consumed);
+		Output output = hash_table[hash_value];
+		printf("Current state: %d Next state: %d Output: %d\n", 
+				current_state, output.next_state, output.output);
+		current_state = output.next_state;
+		i += 1;
+	}	
+}
 
 
 void syntatic_analyser(char reserved_words[N_RESERVED_WORDS][10], char* file_name){
@@ -21,6 +71,7 @@ void syntatic_analyser(char reserved_words[N_RESERVED_WORDS][10], char* file_nam
 
 	while(fgets(line, sizeof(line), f_pointer)){
 		printf("Line: %s\n", line);
+		lexical_analyser(line);
 	}
 	
 }
@@ -28,8 +79,10 @@ void syntatic_analyser(char reserved_words[N_RESERVED_WORDS][10], char* file_nam
 void main(int argc, char** argv){
 	
 	//list of reserved words
-	char reserved_words_list[N_RESERVED_WORDS][10] = {"CONST", "VAR", "PROCEDURE", "BEGIN", "END", "CALL", "WHILE", "DO", "ODD", "IF", "THEN"};	
-	
+	char reserved_words_list[N_RESERVED_WORDS][10] = {"CONST""VAR", "PROCEDURE", "BEGIN", "END", "CALL", "WHILE", "DO", "ODD", "IF", "THEN"};	
+	char consumable_characters[N_CONSUMABLE_CHARS][10] = {" "};
+	char operators[N_OPERATORS][10] = {"+", "-", "*", "=", ">=", "<=", "=="};
+
 	if(argc != 2){
 		printf("ERROR! This analyser takes one and only one argument, which is the file name!\n");
 		exit(1);
