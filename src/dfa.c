aca1ef63c0f412ad1a4493863111f1a6e793d0f7
Gabriel-Balbao <gabrielb.bazon@usp.br>
2025-04-27 18:29:24 -0300

    delete useless files and clean the code

--- src/dfa.c
+++ src/dfa.c
@@ -9,15 +9,21 @@ int hash_function(int state, char char_consumed){
 }
 
 void apply_specific_rules(char *symbol, Output output, Output* transition_vector){
-	
 	if(!strcmp(symbol, "dígito")){
 		for(int i = 48; i <= 57; i++){
 			transition_vector[i] = output;
 		}	
 	}else if(!strcmp(symbol, "letra")){
-		//upper case
 		for(int i = 0; i <= 255; i++){
-			if(isalpha(i)) transition_vector[i] = output;
+			if(isalpha(i)){
+				Output output_new_final;
+				output_new_final.is_final = output.is_final;
+				output_new_final.next_state = output.next_state;
+				
+				output_new_final.output = malloc(sizeof(char) * 70);
+				sprintf(output_new_final.output, "%c", i);
+				transition_vector[i] = output_new_final;
+			}
 		}	
 	}else if(!strcmp(symbol, "outro_=")){
 		for(int i = 0; i <= 255; i++){
@@ -27,6 +33,7 @@ void apply_specific_rules(char *symbol, Output output, Output* transition_vector
 	}else if(!strcmp(symbol, "outro_}")){
 		for(int i = 0; i <= 255; i++){
 			if(i == 125) continue;
+			if(i == '\n') continue;
 			transition_vector[i] = output;
 		}
 	}else if(!strcmp(symbol, "nao_digito")){
@@ -35,20 +42,17 @@ void apply_specific_rules(char *symbol, Output output, Output* transition_vector
 			transition_vector[i] = output;
 		}
 	}else if(!strcmp(symbol, "outro")){
-		
-		for (int i = 0; i <= 255; i++) {
-        		if (isalnum(i)){
-            			continue;
-        		}
-       		 transition_vector[i] = output;
-    		}
+		for (int i = 0; i <= 255; i++){
+			if (isalnum(i)) continue;
+			transition_vector[i] = output;
+		}
 	}
 }
 
 char* sanitize_symbol(char* symbol){
 	if(!strcmp(symbol, "virgula")){
 		return ",";
-	}else if(!strcmp(symbol, "line_break")){
+	}else if(!strcmp(symbol, "quebra_linha")){
 		return "\n";
 	}else if(!strcmp(symbol, "tab")){
 		return  "	";
@@ -60,15 +64,15 @@ char* sanitize_symbol(char* symbol){
 
 Output** csv_parser(char* file_name){
 	FILE* file_p = fopen(file_name, "r");
-	Output**transition_matrix = malloc(NBR_STATES*sizeof(Output*));
+	Output** transition_matrix = malloc(NBR_STATES * sizeof(Output*));
 	
-	//allocate memory for transition matrix
+	// allocate memory for transition matrix
 	for(int i = 0; i < NBR_STATES; i++){		
-		transition_matrix[i] = calloc(NBR_CHARS,sizeof(Output));
-		//populate positions
+		transition_matrix[i] = calloc(NBR_CHARS, sizeof(Output));
+		// populate positions
 		for(int j = 0; j < NBR_CHARS; j++){
 			Output not_implemented;
-			not_implemented.output = "LEXICAL ERROR, NOT RECOGNIZED:";
+			not_implemented.output = "<ERRO_CARAC_INVALIDO>";
 			transition_matrix[i][j] = not_implemented;
 		}
 	}
@@ -81,53 +85,28 @@ Output** csv_parser(char* file_name){
 	int lines_read = 0;
 	while(fgets(line, sizeof(line), file_p)){
 		if(lines_read++ == 0) continue; //discard csv header
-		//printf("Line: %s\n", line);
 
 		int current_state = atoi(strtok(line, ","));
 		char *symbol = strtok(NULL, ",");
 		symbol = sanitize_symbol(symbol);
-		if(!strcmp(symbol, "virgula")){
-			symbol = ",";
-		}else if(!strcmp(symbol, "line_break")){
-			symbol = "\n";
-		}else if(!strcmp(symbol, "tab")){
-			symbol = "	";
-		}
+
 		int next_state = atoi(strtok(NULL, ","));
 		char *output = strtok(NULL, ",");
 
 		int is_final = atoi(strtok(NULL, ","));
-		//int hash_key = hash_function(current_state, symbol);
-		//printf("cs: %d sy: %s ns: %d o: %s \n",
-		//       current_state, symbol, next_state, output);
+
 		Output new_output;
 		new_output.next_state = next_state;
 		new_output.is_final = is_final;
 		new_output.output = strdup(output);
-		if(strlen(symbol) == 1){
-			transition_matrix[current_state][symbol[0]] = new_output;
-			//printf("stored: %d | %s\n", transition_matrix[current_state][symbol[0]].next_state, transition_matrix[current_state][symbol[0]].output);
-		}else{
-			apply_specific_rules(symbol, new_output, transition_matrix[current_state]);			
-			//printf("specific rule!\n");
-		}
 
-		//printf("stored: %d | %s", transition_matrix[current_state][symbol].next_state, transition_matrix[current_state][symbol].output);
+		if(strlen(symbol) == 1)
+			transition_matrix[current_state][symbol[0]] = new_output;
+		else
+			apply_specific_rules(symbol, new_output, transition_matrix[current_state]);			
+
 		lines_read += 1;
 	}
 
-	//printf("hash elements: \n");
-	//Output output = transition_matrix[0]['V'];
-	//printf("state 0 read V -> next state: %d output: %si\n", output.next_state, output.output);
-	//Output output2 = transition_matrix[1]['A'];
-	//printf("state 1 read A -> next state: %d output: %si\n", output2.next_state, output2.output);
-
-
 	return transition_matrix;
-}
-
-
-void test_hash(int argv, char** argc){
-	char* file_name = argc[1];
-	csv_parser(file_name);
 }
--- src/main.c
+++ src/main.c
@@ -3,51 +3,36 @@
 #include<string.h>
 #include"headers.h"
 
-
 LexicalOutput lexical_analyser(FILE *f_pointer, Output** transition_matrix){
-	int number = 0;
-	
-	//Initialize hash table
-	
-	//Output output1;
-	//output1.next_state = 1;
-	//output1.output = 'a';
-
-	//hash_table[0] = output1;	
-	
 	int is_final_state = 0;
 	int i = 0;
 	int current_state = 0;
 	char char_consumed;
 	char capture_output[100];
+	capture_output[0] = '\0';
+
 	while((char_consumed = fgetc(f_pointer)) != EOF){
 		//save input
 		char original_char = char_consumed;
-		//if(check_op(char_consumed)){
-		//	char_consumed = '^';
-			//capture_output[i++] = '\0';
-		//}else{
-		capture_output[i] = char_consumed;
-		//}
-		//if(is_letter(char_consumed, transition_matrix[current_state])) char_consumed = '?';
+
 		Output output = transition_matrix[current_state][char_consumed];
-		//printf("read: %c Current state: %d Next state: %d Output: %s\n", 
-		//			char_consumed, current_state, output.next_state, output.output);
-			
+
 		current_state = output.next_state;
 		if(output.is_final){
-
-			if(!strcmp(output.output, "identifier") || !strcmp(output.output, "numero")){
+			if(!strcmp(output.output, "identificador") || !strcmp(output.output, "numero")){
 				capture_output[i] = '\0';
 				ungetc(original_char, f_pointer);
 			}else{
-				capture_output[++i] = '\0';
+				if(capture_output[i] == ' ' || capture_output[i] == '\n' || capture_output[i] == '\t') capture_output[i] = '\0';
+				else capture_output[++i] = '\0';
 			}
 			LexicalOutput lexical_output;
 			strcpy(lexical_output.entity, output.output);
 			strcpy(lexical_output.value, capture_output);
 			lexical_output.end = 0;	
 			return lexical_output;
+		} else{
+			strcat(capture_output, output.output);
 		}
 
 		i++;
@@ -60,7 +45,6 @@ LexicalOutput lexical_analyser(FILE *f_pointer, Output** transition_matrix){
 
 
 int check_if_reserved_word(char *word, char reserved_words[N_RESERVED_WORDS][10]){
-	
 	for(int i = 0; i < N_RESERVED_WORDS; i++){
 		if(!strcmp(word, reserved_words[i])) return 1;
 	}
@@ -93,12 +77,10 @@ void syntatic_analyser(char reserved_words[N_RESERVED_WORDS][10], char* file_nam
 		LexicalOutput lexical_output = lexical_analyser(f_pointer, transition_matrix);
 		
 		if(lexical_output.end){
-			printf("END OF FILE REACHED!\n");
+			printf("\n========== END OF FILE REACHED! ========= \n");
 			break;
 		}
 
-
-	
 		//check if we want to print the output
 		if(lexical_output.entity[0] == '!') continue;
 		
@@ -107,24 +89,15 @@ void syntatic_analyser(char reserved_words[N_RESERVED_WORDS][10], char* file_nam
 		if(check_if_reserved_word(lexical_output.value, reserved_words)){
 			printf("%s , %s \n", lexical_output.value, lexical_output.value);
 		}else{
-			printf("%s , %s \n", lexical_output.entity, lexical_output.value);
+			printf("%s , %s \n", lexical_output.value, lexical_output.entity);
 		}
 		
 	}
-
-	//while(fgets(line, sizeof(line), f_pointer)){
-	//	printf("Line: %s\n", line);
-	//	lexical_analyser(line, transition_matrix);
-	//}
-	
 }
 
 void main(int argc, char** argv){
-	
-	//list of reserved words
+	// set the list of reserved words
 	char reserved_words_list[N_RESERVED_WORDS][10] = {"CONST", "VAR", "PROCEDURE", "BEGIN", "END", "CALL", "WHILE", "DO", "ODD", "IF", "THEN"};	
-	char consumable_characters[N_CONSUMABLE_CHARS][10] = {" "};
-	char operators[N_OPERATORS][10] = {"+", "-", "*", "=", ">=", "<=", "=="};
 
 	if(argc != 2){
 		printf("ERROR! This analyser takes one and only one argument, which is the file name!\n");
@@ -133,12 +106,7 @@ void main(int argc, char** argv){
 
 	char *file_name = argv[1];
 	char matrix_path[] = "./data/var.csv"; 
-
-	printf("file name: %s\n", file_name);	
 	
-	printf("\n========== STARTING LEXICAL ANALYSIS! =======\n");
+	printf("\n========== STARTING LEXICAL ANALYSIS! ========= \n\n");
 	syntatic_analyser(reserved_words_list, file_name, matrix_path);
-
-
-
 }
