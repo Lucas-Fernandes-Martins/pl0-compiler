e49a92ce177fde185d3af12e30b5c0a97b651965
Gabriel-Balbao <gabrielb.bazon@usp.br>
2025-04-28 00:55:02 -0300

    delete useless files

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 parse:
-	python3 ./src/parser.py ./state_machines/automata_compiler.jff > ./data/var.csv
+	python3 ./state_machines/parser.py ./state_machines/automata_compiler.jff > ./data/transition_matrix.csv
 
 all:
-	gcc -o ./main ./src/main.c ./src/dfa.c ./src/utils.c
+	gcc -o ./main ./src/main.c ./src/transition_matrix_creator.c
 
 run:	all
 	./main  $(FILE_PATH)
--- src/main.c
+++ src/main.c
@@ -1,24 +1,50 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include"headers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "headers.h"
 
-LexicalOutput lexical_analyser(FILE *f_pointer, Output** transition_matrix){
-	int is_final_state = 0;
+static const char reserved_words[NUM_RESERVED_WORDS][MAX_RESERVED_WORD_LENGHT] = {
+    "CONST", "VAR", "PROCEDURE", "BEGIN", "END",
+    "CALL", "WHILE", "DO", "ODD", "IF", "THEN"
+};
+
+LexicalOutput lexical_analyser(FILE *f_pointer, Transition** transition_matrix){
+	State current_state;
+	current_state.number = 0;
+	current_state.is_final = 0;
+
+	LexicalOutput lexical_output;
+
 	int i = 0;
-	int current_state = 0;
 	char char_consumed;
-	char capture_output[100];
+	char capture_output[128];
 	capture_output[0] = '\0';
 
 	while((char_consumed = fgetc(f_pointer)) != EOF){
 		//save input
 		char original_char = char_consumed;
 
-		Output output = transition_matrix[current_state][char_consumed];
+		Transition current_transition = transition_matrix[current_state.number][char_consumed];
 
-		current_state = output.next_state;
-		if(output.is_final){
+		printf("%s\n", current_transition.output);
+
+		/*
+		if(current_state.is_final == 1){
+			break;
+		}
+
+		lexical_output.token[i] = current_transition.output[0];
+		current_state = current_transition.next_state;
+
+		
+		
+			//check if identifier is reserved word		
+			if(check_if_reserved_word(lexical_output.value)){
+				printf("%s , %s \n", lexical_output.value, lexical_output.value);
+			}else{
+				printf("%s , %s \n", lexical_output.value, lexical_output.entity);
+			}
+
 			if(!strcmp(output.output, "identificador") || !strcmp(output.output, "numero")){
 				capture_output[i] = '\0';
 				ungetc(original_char, f_pointer);
@@ -35,17 +61,16 @@ LexicalOutput lexical_analyser(FILE *f_pointer, Output** transition_matrix){
 			strcat(capture_output, output.output);
 		}
 
+		*/
 		i++;
 	}
 	
-	LexicalOutput end_output;
-	end_output.end = 1;
-	return end_output;
+	return lexical_output;
 }
 
 
-int check_if_reserved_word(char *word, char reserved_words[N_RESERVED_WORDS][10]){
-	for(int i = 0; i < N_RESERVED_WORDS; i++){
+int check_if_reserved_word(char *word){
+	for(int i = 0; i < NUM_RESERVED_WORDS; i++){
 		if(!strcmp(word, reserved_words[i])) return 1;
 	}
 
@@ -54,26 +79,20 @@ int check_if_reserved_word(char *word, char reserved_words[N_RESERVED_WORDS][10]
 }
 
 
-void syntatic_analyser(char reserved_words[N_RESERVED_WORDS][10], char* file_name, char* matrix_path){
-
+void syntatic_analyser(char* file_name, char* matrix_path){
 	//Open file
 	FILE *f_pointer;
 	f_pointer = fopen(file_name, "r");
-	//we're reading line by line, need to establish
-	//max number of a line
-	int batch_size = 100;
-	char line[batch_size];
 
 	if(f_pointer == NULL){
 		printf("Error: Invalid file!\n");
 		exit(1);
 	}
+
 	//Load transition table
-	Output **transition_matrix = csv_parser(matrix_path);
+	Transition **transition_matrix = csv_parser(matrix_path);
 	
-	int i = 0;
 	while(1){
-		
 		LexicalOutput lexical_output = lexical_analyser(f_pointer, transition_matrix);
 		
 		if(lexical_output.end){
@@ -81,23 +100,12 @@ void syntatic_analyser(char reserved_words[N_RESERVED_WORDS][10], char* file_nam
 			break;
 		}
 
-		//check if we want to print the output
-		if(lexical_output.entity[0] == '!') continue;
-		
-		//check if identifier is reserved word		
-		
-		if(check_if_reserved_word(lexical_output.value, reserved_words)){
-			printf("%s , %s \n", lexical_output.value, lexical_output.value);
-		}else{
-			printf("%s , %s \n", lexical_output.value, lexical_output.entity);
-		}
-		
+		// printf("%s, %s \n", lexical_output.token, lexical_output.class);
 	}
 }
 
 void main(int argc, char** argv){
 	// set the list of reserved words
-	char reserved_words_list[N_RESERVED_WORDS][10] = {"CONST", "VAR", "PROCEDURE", "BEGIN", "END", "CALL", "WHILE", "DO", "ODD", "IF", "THEN"};	
 
 	if(argc != 2){
 		printf("ERROR! This analyser takes one and only one argument, which is the file name!\n");
@@ -105,8 +113,10 @@ void main(int argc, char** argv){
 	}
 
 	char *file_name = argv[1];
-	char matrix_path[] = "./data/var.csv"; 
+	char matrix_path[] = "./data/transition_matrix.csv"; 
+
+	//Transition **transition_matrix = csv_parser(matrix_path);
 	
 	printf("\n========== STARTING LEXICAL ANALYSIS! ========= \n\n");
-	syntatic_analyser(reserved_words_list, file_name, matrix_path);
+	syntatic_analyser(file_name, matrix_path);
 }
